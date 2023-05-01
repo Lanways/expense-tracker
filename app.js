c13a116e266f2525cfe79a04903a634ab6594955
Lanways <mcbluedd@gmail.com>
2023-05-01 15:36:54 +0800

    Initialized project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
+})
