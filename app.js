07c30d79fe79e5ed8599291391bcc9a4feda8c9d
Lanways <mcbluedd@gmail.com>
2023-05-01 23:23:10 +0800

    feat: add body-parser and create record function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const Record = require('./models/record')
+const bodyParser = require('body-parser')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -12,9 +15,28 @@ const port = process.env.PORT
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .then(records => res.render('index', { records }))
+    .catch(err => console.log(err))
 })
+
+app.post('/records', (req, res) => {
+  // const name = req.user._id
+  console.log(req.body)
+  return Record.create({ ...req.body })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
+app.get('/records/new', (req, res) => {
+  res.render('new')
+})
+
+
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
