c829d8e17de9b8e3b27b04d8726c66462d60e947
Lanways <mcbluedd@gmail.com>
2023-05-03 14:25:43 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -29,6 +29,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
 usePassport(app)
+app.use((req, res, next) => {
+  //locals的data所有view都可以存取
+  //把req.isAuthenticated()回傳的boolean給res
+  res.locals.isAuthenticated = req.isAuthenticated()
+  //反序列化 取出的 user給res
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
