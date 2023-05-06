ce02499ae52452e3c987a335a05ea8141475742d
Lanways <mcbluedd@gmail.com>
2023-05-06 21:29:40 +0800

    feat: modify MOGODB_URI

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const app = express()
-const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-const usePassport = require('./config/passport')
-const routes = require('./routes')
-const session = require('express-session')
-const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const usePassport = require('./config/passport')
+const session = require('express-session')
+const flash = require('connect-flash')
+const routes = require('./routes')
+
 require('./config/mongoose')
 
-const Port = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({
   defaultLayout: 'main',
@@ -52,6 +53,6 @@ app.use((req, res, next) => {
 })
 app.use(routes)
 
-app.listen(Port, () => {
-  console.log(`Express is running on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Express is running on http://localhost:${PORT}`)
 })
